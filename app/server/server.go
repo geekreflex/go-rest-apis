c6401c362f6e7e4b74b292a75942b77a55d85adb
jerry <jerrynwosu007@gmail.com>
2023-04-07 18:14:32 +0100

    init application

---
+++ app/server/server.go
@@ -0,0 +1,109 @@
+package server
+
+import (
+	"goly/model"
+	"goly/utils"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func GetAllGolies(ctx *fiber.Ctx) error {
+	golies, err := model.GetAllGolies()
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
+			"message": "error getting all goly links " + err.Error(),
+		})
+	}
+	return ctx.Status(fiber.StatusOK).JSON(golies)
+}
+
+func getGoly(c *fiber.Ctx) error {
+	id, err := strconv.ParseUint(c.Params("id"), 10, 64)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
+			"message": "error could not parse id " + err.Error(),
+		})
+	}
+
+	goly, err := model.GetGoly(id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
+			"message": "error could not retreive goly from db " + err.Error(),
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(goly)
+}
+
+func createGoly(c *fiber.Ctx) error {
+	c.Accepts("application/json")
+
+	var goly model.Goly
+	err := c.BodyParser(&goly)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
+			"message": "error parsing JSON " + err.Error(), 
+		})
+	}
+
+	if goly.Random {
+		goly.Goly = utils.RamdomURL(8)
+	}
+
+	err = model.CreateGol(goly)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
+			"message": "could not creare goly in db " + err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(goly)
+}
+
+func updateGoly(c *fiber.Ctx) error {
+	c.Accepts("application/json")
+
+	var goly model.Goly
+
+	err := c.BodyParser(&goly)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
+			"message": "could not parse json " + err.Error(),
+		})
+	}
+
+	err = model.UpdateGoly(goly)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
+			"message": "could not update goly link in DB " + err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(goly)
+}
+
+func deleteGoly (c *fiber.Ctx) error {
+	id, err := strconv.ParseUint(c.Params("id"), 10, 64)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
+			"message": "could not parse id from url " + err.Error()
+		})
+	}
+}
+
+func SetupAndListen() {
+	router := fiber.New()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	router.Get("/goly", GetAllGolies)
+	router.Get("/goly/:id", getGoly)
+	router.Post("/goly", createGoly)
+	router.Patch("/goly", updateGoly)
+
+	router.Listen(":3000")
+}
